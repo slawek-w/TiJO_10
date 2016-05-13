4be5861266b4e4e4227b8d1d9854c0c3e7a5c4d3
slawek-w <swiel@wp.pl>
2016-05-13 12:15:39 +0200

    new tests in app.spec.js

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -2,6 +2,7 @@ describe('app', function () {
     'use strict';
 
     var app = window.app;
+    var answer = window.app;
 
     describe('assignToSwimmingCourse', function () {
         it('should assign adult person to adult group', function () {
@@ -18,6 +19,37 @@ describe('app', function () {
             expect(app.assignToSwimmingCourse('Zack', '10/06/2010')).toEqual({
                 name: 'Zack', age: 5, course: 'kids'
             });
+        });
+        it('should assign person below 12 to kids group', function () {
+            expect(app.assignToSwimmingCourse('Nick', '5/10/2012')).toEqual({
+                name: 'Zack', age: 4, course: 'kids'
+            });
+        });
+        it('should assign adult person to adult group', function () {
+            expect(app.assignToSwimmingCourse('Tom', '17/02/1992')).toEqual({
+                name: 'Zack', age: 24, course: 'adults'
+            });
+        });
+    });
+
+    describe('calculateArea', function () {
+        it('should return false when first element is not a number', function () {
+            expect(answer.calculateArea('a', 10, 10, 'Sukces', 'Niepowodzenie')).toBe(false);
+        });
+        it('should return false when second element is not a number', function () {
+            expect(answer.calculateArea(10, 'a', 10, 'Sukces', 'Niepowodzenie')).toBe(false);
+        });
+        it('should return false when third element is not a number', function () {
+            expect(answer.calculateArea(10, 10, 'a', 'Sukces', 'Niepowodzenie')).toBe(false);
+        });
+        it('should return false when fourth element is not a string', function () {
+            expect(answer.calculateArea(10, 10, 10, 123, 'Niepowodzenie')).toBe(false);
+        });
+        it('should return false when fifth element is not a string', function () {
+            expect(answer.calculateArea(10, 10, 10, 'Sukces', 123)).toBe(false);
+        });
+        it('should return false when not enough elementss', function () {
+            expect(answer.calculateArea(10, 10)).toBe(false);
         });
     });
 });
